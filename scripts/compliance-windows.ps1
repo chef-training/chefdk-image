a26f1b8acc8bbfcfafe2f07bdc598e3cfce7f083
Franklin Webber <franklin.webber@gmail.com>
2016-02-10 15:47:18 -0600

    Add the Compliance Windows Node (WARNING)
    I am adding the Packer file here to create this AMI. However, this packer script does not currently work and fails when attempting to install components through the Nordstrom setup script that is part of this pprocess. This may be fixed in a future version of Packer or I may explore some changes to the install script to make it work. Until then this packer script really does the job of showing it it would look if it worked. This may be more confusing for someone or a mistake. I'm sorry if this burns you.
    
    In other news, the compliance-windows powershell script sets the state of the system up when you do want to create a new AMI. Well everything except for the WinRM script that also need to be run!

---
+++ scripts/compliance-windows.ps1
@@ -0,0 +1,34 @@
+Write-Output "Setting Administrator Password"
+([ADSI]'WinNT://localhost/Administrator, user').psbase.Invoke('SetPassword', 'Cod3Can!')
+
+Write-Output "Creating Hints Location and Writing Hints File"
+New-Item -path 'C:\\chef\\ohai\\hints' -type directory
+Add-Content 'C:\\chef\\ohai\\hints\\ec2.json' '{}'
+
+Write-Output "Writing out Kitchen Template"
+Invoke-WebRequest "https://raw.githubusercontent.com/chef-training/chef-essentials-windows/master/kitchen-template.yml" -OutFile 'C:\\Users\\Administrator\kitchen-template.yml'
+
+# NOTE: This currently fails when executed through Packer - Powershell
+Write-Output "Execute Nordstrom ChefDK Bootstrap"
+(Invoke-WebRequest -Uri https://raw.githubusercontent.com/Nordstrom/chefdk_bootstrap/master/bootstrap.ps1).Content | Invoke-Expression
+
+Write-Output "Installing Kitchen EC2 Gem"
+Invoke-Expression "chef gem install kitchen-ec2"
+
+# Install ConEmu
+Invoke-Expression "C:\ProgramData\chocolatey\bin\choco install conemu -y"
+
+# Append Chef Development Kit (ChefDK) Embedded Bin to the PATH
+
+$LocalMachinePathRegKey = 'HKLM:\System\CurrentControlSet\Control\Session Manager\Environment'
+$ChefDkEmbeddedBin = 'C:\opscode\chefdk\embedded\bin'
+
+Push-Location
+Set-Location $LocalMachinePathRegKey
+$CurrentPath = (Get-ItemProperty . Path).Path
+$PathWithInSpec = "$CurrentPath;$ChefDkEmbeddedBin"
+Set-ItemProperty . -Name Path -Value $PathWithInSpec
+Pop-Location
+
+# Update the InSpec Version to one that is known to work on Windows
+Invoke-Expression "chef gem install inspec -v 0.10.1"
