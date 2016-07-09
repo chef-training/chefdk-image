f9fdc128990017a9581df7c62d5002859ccc365b
Franklin Webber <franklin.webber@gmail.com>
2016-07-09 02:33:48 -0500

    Updates the compliance version

--- cookbooks/workstations/recipes/compliance.rb
+++ cookbooks/workstations/recipes/compliance.rb
@@ -4,6 +4,15 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+#
+# Ensure the package repository is all up-to-date. This is essential
+# because sometimes the packages will fail to install because of a
+# stale package repository.
+#
+# @note This command is not idempotent. A better command may existin within the yum cookbook.
+#
+execute "yum update -y"
+
 chef_ingredient 'chefdk' do
   action :install
   channel :stable
@@ -17,16 +26,8 @@ include_recipe "#{cookbook_name}::centos-chef_user"
 #       required to get the right user to have it installed. Without it the root
 #       user was getting the kitchen-docker gem
 #
-execute 'sudo su -c "chef exec gem install inspec -v 0.14.07" -s /bin/sh chef'
+execute 'sudo su -c "chef exec gem install inspec -v 0.22.1" -s /bin/sh chef'
 
-#
-# Ensure the package repository is all up-to-date. This is essential
-# because sometimes the packages will fail to install because of a
-# stale package repository.
-#
-# @note This command is not idempotent. A better command may existin within the yum cookbook.
-#
-execute "yum update -y"
 
 needed_packages_for_attendees = %w[ vim nano emacs git tree ]
 package needed_packages_for_attendees
