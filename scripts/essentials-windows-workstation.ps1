0e736cee658f09ff4588f26aa644a423f2a1a8d9
Franklin Webber <franklin.webber@gmail.com>
2016-09-08 15:59:07 -0500

    Updates the workstation setup to grab a public template
    The last template is no longer consistently public. This is now fixed by including the template within this project.

--- scripts/essentials-windows-workstation.ps1
+++ scripts/essentials-windows-workstation.ps1
@@ -6,11 +6,12 @@ New-Item -path 'C:\\chef\\ohai\\hints' -type directory
 Add-Content 'C:\\chef\\ohai\\hints\\ec2.json' '{}'
 
 Write-Output "Writing out Kitchen Template"
-Invoke-WebRequest "https://raw.githubusercontent.com/chef-training/chef-essentials-windows/master/kitchen-template.yml" -OutFile 'C:\\Users\\Administrator\kitchen-template.yml'
+Invoke-WebRequest "https://raw.githubusercontent.com/chef-training/chefdk-image/master/test_kitchen-templates/kitchen-template.yml" -OutFile 'C:\\Users\\Administrator\kitchen-template.yml'
 
 # NOTE: This currently fails when executed through Packer - Powershell
 Write-Output "Execute Nordstrom ChefDK Bootstrap"
-(Invoke-WebRequest -Uri https://raw.githubusercontent.com/Nordstrom/chefdk_bootstrap/master/bootstrap.ps1).Content | Invoke-Expression
+Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/Nordstrom/chefdk_bootstrap/master/bootstrap.ps1 | Invoke-Expression
+install
 
 Write-Output "Installing Kitchen EC2 Gem"
 Invoke-Expression "chef gem install kitchen-ec2"
