6dec746ecf73ad9be1354c62d576772e0ebbdc62
Franklin Webber <franklin.webber@gmail.com>
2017-10-25 23:23:40 -0500

    Update intermediate workstations
    Signed-off-by: Franklin Webber <franklin@chef.io>

--- cookbooks/workstations/recipes/intermediate.rb
+++ cookbooks/workstations/recipes/intermediate.rb
@@ -9,32 +9,27 @@
 # The TDD course takes care of the core requirements for the environment
 include_recipe "#{cookbook_name}::tdd_cookbook_development"
 
-directory '/home/chef/day_2' do
-  user 'chef'
-  group 'chef'
-end
-
-git '/home/chef/extending_cookbooks-repo' do
-  repository 'https://github.com/chef-training/extending_cookbooks-repo.git'
+git '/home/chef/chef-intermediate-repo' do
+  repository 'https://github.com/chef-training/chef-intermediate-repo.git'
   revision 'master'
   user 'chef'
   group 'chef'
   notifies :run, 'execute[copy cookbook to home]', :immediately
 end
 
-directory '/home/chef/extending_cookbooks-repo' do
+directory '/home/chef/chef-intermediate-repo' do
   recursive true
   action :nothing
 end
 
 execute 'copy cookbook to home' do
-  command 'cp -R /home/chef/extending_cookbooks-repo/cookbooks/httpd /home/chef/day_2/httpd'
+  command 'cp -R /home/chef/chef-intermediate-repo/apache /home/chef/apache'
   action :nothing
   notifies :run, 'execute[set correct permissions on cookbook]', :immediately
 end
 
 execute 'set correct permissions on cookbook' do
-  command 'chown -R chef:chef /home/chef/day_2/httpd'
+  command 'chown -R chef:chef /home/chef/apache'
   action :nothing
-  notifies :delete, 'directory[/home/chef/extending_cookbooks-repo]', :immediately
+  notifies :delete, 'directory[/home/chef/chef-intermediate-repo]', :immediately
 end
