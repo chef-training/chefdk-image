e63cdedac0f978302bea524c10cc014cde4dc4ff
Franklin Webber <franklin.webber@gmail.com>
2017-11-13 15:28:19 -0600

    Updates Chef Intermediate to latest verison of setup
    Signed-off-by: Franklin Webber <franklin@chef.io>

--- cookbooks/workstations/recipes/intermediate.rb
+++ cookbooks/workstations/recipes/intermediate.rb
@@ -9,27 +9,13 @@
 # The TDD course takes care of the core requirements for the environment
 include_recipe "#{cookbook_name}::tdd_cookbook_development"
 
-git '/home/chef/chef-intermediate-repo' do
+git '/home/chef/apache' do
   repository 'https://github.com/chef-training/chef-intermediate-repo.git'
   revision 'master'
   user 'chef'
   group 'chef'
-  notifies :run, 'execute[copy cookbook to home]', :immediately
-end
-
-directory '/home/chef/chef-intermediate-repo' do
-  recursive true
-  action :nothing
-end
-
-execute 'copy cookbook to home' do
-  command 'cp -R /home/chef/chef-intermediate-repo/apache /home/chef/apache'
-  action :nothing
-  notifies :run, 'execute[set correct permissions on cookbook]', :immediately
 end
 
 execute 'set correct permissions on cookbook' do
   command 'chown -R chef:chef /home/chef/apache'
-  action :nothing
-  notifies :delete, 'directory[/home/chef/chef-intermediate-repo]', :immediately
 end
